aa2a723d96ba219e89be2c79e05ac5ed36cdf346
huangwenchuan <wenchuan.huang@rjoygame.com>
2019-08-19 15:51:20 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <State/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import State from './components/gojs/State'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    State
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
